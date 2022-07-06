4d343c9d558849b5a990ac596ca2395e14d3302c
mayurwaB <bodlexnig@outlook.com>
2022-07-06 21:17:25 +0100

    Initial repo 1

--- roles/print/files/script.sh
+++ roles/print/files/script.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-
-sudo useradd --no-create-home prometheus
-sudo mkdir /etc/prometheus
-sudo mkdir /var/lib/prometheus
-
-#download and extract prometheus
-wget "https://github.com/prometheus/prometheus/releases/download/v2.36.2/prometheus-2.36.2.linux-amd64.tar.gz"
-tar xvfz prometheus-2.36.2.linux-amd64.tar.gz
-
-#copy binaries to local binaries folder
-sudo cp prometheus-2.36.2.linux-amd64/prometheus /usr/local/bin/
-sudo cp prometheus-2.36.2.linux-amd64/promtool /usr/local/bin/
-
-#copy config to config directory
-sudo cp -r prometheus-2.36.2.linux-amd64/consoles /etc/prometheus/
-sudo cp -r prometheus-2.36.2.linux-amd64/console_libraries /etc/prometheus/
-sudo cp prometheus-2.36.2.linux-amd64/prometheus.yml /etc/prometheus/
-
-#remove residual files
-rm -rf prometheus-2.36.2.linux-amd64.tar.gz prometheus-2.36.2.linux-amd64
-
-#configure ownership
-sudo chown prometheus:prometheus /etc/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/prometheus
-sudo chown prometheus:prometheus /usr/local/bin/promtool
-sudo chown -R prometheus:prometheus /etc/prometheus/consoles
-sudo chown -R prometheus:prometheus /etc/prometheus/console_libraries
+# Using Ubuntu
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 
